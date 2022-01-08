00447f72f35e5e5f80bd35868091ab8176186f93
ericflatt <eflatt18@gmail.com>
2022-01-07 22:06:15 -0800

    scss update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header container">
+    
+        <h1>
+          Hello!
+        </h1>
       </header>
     </div>
   );
 }
 
+
+console.log("background photo credit https://crosscut.com/2019/09/neon-symbol-seattles-past-what-will-illuminate-its-future")
 export default App;
