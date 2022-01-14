55357bb55c284c18a05dcd977a2c11e1bf49f214
ericflatt <eflatt18@gmail.com>
2022-01-13 21:01:30 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import "./App.scss";
 import About from "./components/about";
 // import Marquees from "./components/marquee";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faLinkedinIn, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
-    <div  className="App">
+    <div className="App">
       {/* <div className="noise"></div> */}
       <header className="App-header container">
         <h1>Hello!</h1>
         <p>
           I'm Eric. Currently I am a designer on the digital experience team at
           Funko.
-        </p>  
+        </p>
+        <div className="social-icons">
+         <a href="https://instagram.com/eric_flatt" target="blank"> <FontAwesomeIcon icon={faInstagram} size="lg" /> </a>
+         <a href="https://twitter.com/__flatt" target="blank"> <FontAwesomeIcon icon={faTwitter} size="lg" /> </a>
+         <a href="https://linkedin.com/in/ericbflatt" target="blank"> <FontAwesomeIcon icon={faLinkedinIn} size="lg" /> </a>
+        </div>
       </header>
+
       <About />
       {/* <Marquees /> */}
     </div>
