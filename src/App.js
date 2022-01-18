dee2d6cba631b65f0141d3513f52f15e50b40f60
ericflatt <eflatt18@gmail.com>
2022-01-17 17:38:32 -0800

    ga maybe

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import About from "./components/about";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faLinkedinIn, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
+import ReactGA from 'react-ga';
+
+const trackingId = "UA-107339411-2"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.set({
+  // userId: auth.currentUserId(),
+  // any data that is relevant to the user session
+  // that you would like to track with google analytics
+})
+
 function App() {
   return (
     <div className="App">
